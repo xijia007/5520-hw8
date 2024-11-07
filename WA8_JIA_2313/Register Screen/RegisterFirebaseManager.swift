375fe596718dbc1390d88b6e01ef8282ee827b41
xijia007 <jia.xi1@northeastern.edu>
2024-11-06 21:08:54 -0800

    check validation for login and signup

--- WA8_JIA_2313/Register Screen/RegisterFirebaseManager.swift
+++ WA8_JIA_2313/Register Screen/RegisterFirebaseManager.swift
@@ -1,86 +1,143 @@
+////
+////  RegisterFirebaseManager.swift
+////  WA8_JIA_2313
+////
+////  Created by Xi Jia on 11/5/24.
+////
 //
-//  RegisterFirebaseManager.swift
-//  WA8_JIA_2313
+//import Foundation
+//import FirebaseAuth
+//import FirebaseFirestore
 //
-//  Created by Xi Jia on 11/5/24.
+//extension RegisterViewController{
+//    
+//    
+//    
+//    func registerNewAccount(){
+//        //MARK: display the progress indicator...
+//        showActivityIndicator()
+//        //MARK: create a Firebase user with email and password...
+//        if let name = registerView.textFieldName.text,
+//           let email = registerView.textFieldEmail.text,
+//           let password = registerView.textFieldRepeatPassword.text{
+//            //Validations....
+//            Auth.auth().createUser(withEmail: email, password: password, completion: {result, error in
+//                if error == nil{
+//                    //MARK: the user creation is successful...
+//                    self.setNameOfTheUserInFirebaseAuth(name: name)
+//                }else{
+//                    //MARK: there is a error creating the user...
+////                    print(error)
+//                }
+//                guard let user = result?.user else{return}
+//                
+//                // User created successfully, now add to Firestore
+//                self.addUserToFirestore(user: user, name: name)
+//            })
+//        }
+//    }
+//    
+//    func addUserToFirestore(user: User, name: String) {
+//        let db = Firestore.firestore()
+//        let userRef = db.collection("users").document(user.uid)
+//        
+//        let userData: [String: Any] = [
+//            "name": name,
+//            "email": user.email ?? "",
+//            "uid": user.uid
+//        ]
+//        
+//        userRef.setData(userData) { [weak self] error in
+//            guard let self = self else { return }
+//            
+//            if let error = error {
+//                print("Error adding user to Firestore: \(error.localizedDescription)")
+//            } else {
+//                print("User successfully added to Firestore")
+//                self.setNameOfTheUserInFirebaseAuth(name: name)
+//            }
+//            
+//            self.hideActivityIndicator()
+//            // Navigate to the next screen or perform any other action
+//            // self.navigationController?.popViewController(animated: true)
+//        }
+//    }
+//
+//    
+//    //MARK: We set the name of the user after we create the account...
+//    func setNameOfTheUserInFirebaseAuth(name: String){
+//        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+//        changeRequest?.displayName = name
+//        changeRequest?.commitChanges(completion: {(error) in
+//            if error == nil{
+//                //MARK: hide the progress indicator...
+//                self.hideActivityIndicator()
+//                //MARK: the profile update is successful...
+//                self.navigationController?.popViewController(animated: true)
+//            }else{
+//                //MARK: there was an error updating the profile...
+//                print("Error occured: \(String(describing: error))")
+//            }
+//        })
+//    }
+//    
+//    
+//}
 //
 
 import Foundation
 import FirebaseAuth
 import FirebaseFirestore
 
-extension RegisterViewController{
+class RegisterFirebaseManager{
+    let db = Firestore.firestore()
     
-    
-    
-    func registerNewAccount(){
-        //MARK: display the progress indicator...
-        showActivityIndicator()
-        //MARK: create a Firebase user with email and password...
-        if let name = registerView.textFieldName.text,
-           let email = registerView.textFieldEmail.text,
-           let password = registerView.textFieldRepeatPassword.text{
-            //Validations....
-            Auth.auth().createUser(withEmail: email, password: password, completion: {result, error in
-                if error == nil{
-                    //MARK: the user creation is successful...
-                    self.setNameOfTheUserInFirebaseAuth(name: name)
-                }else{
-                    //MARK: there is a error creating the user...
-                    print(error)
+    func checkExistingUser(email: String, name: String, completion: @escaping (Bool, String) -> Void) {
+        // Check if email exists
+        Auth.auth().fetchSignInMethods(forEmail: email) { (methods, error) in
+            if methods != nil {
+                completion(true, "Email")
+                return
+            }
+            
+            // Check if name exists
+            self.db.collection("users").whereField("name", isEqualTo: name).getDocuments { (snapshot, error) in
+                if let documents = snapshot?.documents, !documents.isEmpty {
+                    completion(true, "Name")
+                } else {
+                    completion(false, "")
                 }
-                guard let user = result?.user else{return}
-                
-                // User created successfully, now add to Firestore
-                self.addUserToFirestore(user: user, name: name)
-            })
+            }
         }
     }
     
-    func addUserToFirestore(user: User, name: String) {
-        let db = Firestore.firestore()
-        let userRef = db.collection("users").document(user.uid)
-        
-        let userData: [String: Any] = [
-            "name": name,
-            "email": user.email ?? "",
-            "uid": user.uid
-        ]
-        
-        userRef.setData(userData) { [weak self] error in
-            guard let self = self else { return }
-            
+    func registerUser(name: String, email: String, password: String, completion: @escaping (AuthDataResult?) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             if let error = error {
-                print("Error adding user to Firestore: \(error.localizedDescription)")
-            } else {
-                print("User successfully added to Firestore")
-                self.setNameOfTheUserInFirebaseAuth(name: name)
+                print("Error creating user: \(error.localizedDescription)")
+                completion(nil)
+                return
             }
             
-            self.hideActivityIndicator()
-            // Navigate to the next screen or perform any other action
-            // self.navigationController?.popViewController(animated: true)
+            guard let user = authResult?.user else {
+                completion(nil)
+                return
+            }
+            
+            let userData: [String: Any] = [
+                "name": name,
+                "email": email,
+                "uid": user.uid
+            ]
+            
+            self.db.collection("users").document(user.uid).setData(userData) { error in
+                if let error = error {
+                    print("Error saving user data: \(error.localizedDescription)")
+                    completion(nil)
+                } else {
+                    completion(authResult)
+                }
+            }
         }
     }
-
-    
-    //MARK: We set the name of the user after we create the account...
-    func setNameOfTheUserInFirebaseAuth(name: String){
-        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
-        changeRequest?.displayName = name
-        changeRequest?.commitChanges(completion: {(error) in
-            if error == nil{
-                //MARK: hide the progress indicator...
-                self.hideActivityIndicator()
-                //MARK: the profile update is successful...
-                self.navigationController?.popViewController(animated: true)
-            }else{
-                //MARK: there was an error updating the profile...
-                print("Error occured: \(String(describing: error))")
-            }
-        })
-    }
-    
-    
 }
-
--- WA8_JIA_2313/Register Screen/RegisterViewController.swift
+++ WA8_JIA_2313/Register Screen/RegisterViewController.swift
@@ -1,49 +1,135 @@
+////
+////  RegisterViewController.swift
+////  WA8_JIA_2313
+////
+////  Created by Xi Jia on 11/5/24.
+////
 //
-//  RegisterViewController.swift
-//  WA8_JIA_2313
 //
-//  Created by Xi Jia on 11/5/24.
+//import UIKit
+//import FirebaseAuth
+//import FirebaseFirestore
 //
-
+//class RegisterViewController: UIViewController {
+//    
+//    let childProgressView = ProgressSpinnerViewController()
+//
+//    let registerView = RegisterView()
+//    
+//    override func loadView() {
+//        view = registerView
+//    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        navigationController?.navigationBar.prefersLargeTitles = true
+//        registerView.buttonRegister.addTarget(self, action: #selector(onRegisterTapped), for: .touchUpInside)
+//        title = "Register"
+//    }
+//    
+//    @objc func onRegisterTapped(){
+//        let password = registerView.textFieldPassword.text
+//        let repeatPassword = registerView.textFieldRepeatPassword.text
+//
+//        if password != repeatPassword {
+//            showAlert(message: "Passwords do not match!")
+//            return
+//        }
+//        //MARK: creating a new user on Firebase...
+//        registerNewAccount()
+//    }
+//    
+//    // func to show alert
+//    func showAlert(message: String) {
+//        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+//        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//        self.present(alert, animated: true, completion: nil)
+//    }
+//    
+//    
+//}
 
 import UIKit
 import FirebaseAuth
-import FirebaseFirestore
 
 class RegisterViewController: UIViewController {
     
-    let childProgressView = ProgressSpinnerViewController()
-
     let registerView = RegisterView()
+    let childProgressView = ProgressSpinnerViewController()
+    let registerFirebaseManager = RegisterFirebaseManager()
     
     override func loadView() {
         view = registerView
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         navigationController?.navigationBar.prefersLargeTitles = true
+        
         registerView.buttonRegister.addTarget(self, action: #selector(onRegisterTapped), for: .touchUpInside)
-        title = "Register"
     }
     
     @objc func onRegisterTapped(){
-        let password = registerView.textFieldPassword.text
-        let repeatPassword = registerView.textFieldRepeatPassword.text
-
-        if password != repeatPassword {
+        // Validate inputs
+        guard let name = registerView.textFieldName.text,
+              let email = registerView.textFieldEmail.text,
+              let password = registerView.textFieldPassword.text,
+              let repeatPassword = registerView.textFieldRepeatPassword.text,
+              !name.isEmpty, !email.isEmpty, !password.isEmpty, !repeatPassword.isEmpty else {
+            showAlert(message: "Please fill all fields!")
+            return
+        }
+        
+        // Validate the email format
+        if !isValidEmail(email) {
+            showAlert(message: "Please enter a valid email address.")
+            return
+        }
+        
+        // Check password length
+        guard password.count >= 6 else {
+            showAlert(message: "The password must be 6 characters long or more")
+            return
+        }
+        
+        // Check if passwords match
+        guard password == repeatPassword else {
             showAlert(message: "Passwords do not match!")
             return
         }
-        //MARK: creating a new user on Firebase...
-        registerNewAccount()
+        
+        
+        // Show progress indicator
+        addChild(childProgressView)
+        view.addSubview(childProgressView.view)
+        childProgressView.didMove(toParent: self)
+        
+        // Register user
+        registerFirebaseManager.registerUser(name: name, email: email, password: password){ authResult in
+            DispatchQueue.main.async {
+                self.childProgressView.willMove(toParent: nil)
+                self.childProgressView.view.removeFromSuperview()
+                self.childProgressView.removeFromParent()
+                
+                if let authResult = authResult {
+                    print("Registration successful for user: \(authResult.user.uid)")
+                    self.navigationController?.popViewController(animated: true)
+                } else {
+                    self.showAlert(message: "The email address is already in use by another account.")
+                }
+            }
+        }
     }
     
-    // func to show alert
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
     func showAlert(message: String) {
         let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
-    
-    
 }
